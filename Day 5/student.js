dc04bf63f837ae55475cffff0f4efe7ba8eaf71a
Tusharxhub <t.k.d.dey2033929837@gmail.com>
2026-03-30 12:36:36 +0530

    Add MongoDB query for inserting documents and analyzing execution time with indexing techniques

---
+++ Day 5/student.js
@@ -0,0 +1,8 @@
+//! Write the MongoDB query to insert 1 lakh docs using find & calculate the execution time with indexes & without indexes & perform an analysis within the following techniques:
+//! 1) CollScan
+//! 2) express IXScan
+//! 3) IXScan
+//! Also create/add a unique attribute to the index while creating the index & list all the indexes for their collection.
+
+
+
