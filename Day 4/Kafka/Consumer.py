79ac5a80bba3f19d7c8f2cd12c4f02534fd4f798
Tusharxhub <t.k.d.dey2033929837@gmail.com>
2026-03-30 11:50:39 +0530

    Add Consumer.py and Main.py for real-time data streaming pipeline implementation using Apache Kafka

---
+++ Day 4/Kafka/Consumer.py
@@ -0,0 +1,4 @@
+
+# ! Implement a real-time data streaming pipeline using Apache Kafka. Create a producer that generates random sensor data (e.g., temperature, humidity) and sends it to a kafka to simulate IOT sensor data and store in MongoDB.
+
+
